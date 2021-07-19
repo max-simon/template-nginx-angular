8fc999430e3a026242b37dce084d11e228640ac0
Max Simon <max.simon@ibm.com>
2021-07-19 20:57:54 +0200

    Fixed tests

--- projects/main/src/app/core/services/app-config.service.spec.ts
+++ projects/main/src/app/core/services/app-config.service.spec.ts
@@ -1,12 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { Observable } from 'rxjs';
+import { AppConfig } from './app-config.interface';
 
 import { AppConfigService } from './app-config.service';
+
+const FakeHttpClientService = {
+  get: (url: string) => {
+    return new Observable(observer => {
+      const newAppConfig : AppConfig = {
+        title: "Test title"
+      }
+      observer.next(newAppConfig);
+    })
+  }
+}
 
 describe('AppConfigService', () => {
   let service: AppConfigService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AppConfigService,
+        {provide: HttpClient, useValue: FakeHttpClientService}
+      ]
+    });
     service = TestBed.inject(AppConfigService);
   });
 
--- projects/main/src/app/home/landing-page/landing-page.component.spec.ts
+++ projects/main/src/app/home/landing-page/landing-page.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppConfigService } from '../../core/services/app-config.service';
 
 import { LandingPageComponent } from './landing-page.component';
+
+const appConfigServiceStub: Partial<AppConfigService> = {
+  config: new Promise((resolve, reject) => {
+    resolve({
+      title: "AppConfigService Stub Title"
+    })
+  })
+}
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
@@ -8,7 +17,13 @@ describe('LandingPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingPageComponent ]
+      declarations: [ LandingPageComponent ],
+      providers: [
+        {
+          provide: AppConfigService,
+          useValue: appConfigServiceStub
+        }
+      ]
     })
     .compileComponents();
   });
